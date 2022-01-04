5de51ae8ef1d64d109387d3f9126227c381c708b
joonkeekim <joonkee0701@gmail.com>
2022-01-04 15:56:22 +0900

    add network.py and its test file

---
+++ network.py
@@ -0,0 +1,66 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from embedding import Embedding
+
+
+def activation_mapper(activation):
+    if activation in [None, '']:
+        return nn.Identity()
+    elif isinstance(activation, str):
+        return getattr(torch.nn, activation)
+    elif callable(activation):
+        return activation
+    else:
+        raise ValueError()
+
+
+# TODO: update "use_qat" option
+class NeuralFieldsNetwork(nn.Module):
+    def __init__(self,
+                 in_features: int, out_features: int,
+                 hidden_features: int, n_hidden_layers: int,
+                 input_embedding=None,
+                 activation='ReLU',
+                 output_activation='Sigmoid',
+                 use_qat=False):
+        super(NeuralFieldsNetwork, self).__init__()
+ 
+        self.in_features = in_features
+        self.out_features = out_features
+        self.hidden_features = hidden_features
+        self.n_hidden_layers = n_hidden_layers
+
+        activation = activation_mapper(activation)
+        output_activation = activation_mapper(output_activation)
+
+        self.nets = []
+
+        if input_embedding is not None:
+            assert isinstance(input_embedding, Embedding)
+            self.nets.extend([
+                input_embedding,
+                nn.Linear(input_embedding.get_output_size(),
+                          hidden_features),
+                activation()])
+        else: # input_embedding is None
+            self.nets.extend([
+                nn.Linear(in_features, hidden_features),
+                activation()])
+
+        for i in range(self.n_hidden_layers):
+            self.nets.extend([
+                nn.Linear(hidden_features, hidden_features),
+                activation()])
+
+        self.nets.extend([
+            nn.Linear(hidden_features, out_features),
+            output_activation()])
+
+        self.nets = nn.Sequential(*self.nets)
+
+    def forward(self, inputs):
+        return self.nets(inputs)
+
---
+++ network_test.py
@@ -0,0 +1,19 @@
+import torch
+import unittest
+from network import *
+
+
+class NetworkTest(unittest.TestCase):
+    def test_neural_fields_network(self):
+        in_features, out_features = 3, 5
+        hidden_features, n_hidden_layers = 64, 3
+
+        network = NeuralFieldsNetwork(in_features, out_features,
+                                      hidden_features, n_hidden_layers)
+        self.assertEqual(network(torch.ones([32, in_features])).size(),
+                         (32, out_features))
+
+
+if __name__ == '__main__':
+    unittest.main()
+
